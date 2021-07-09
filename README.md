aab371525b5b9e568b91f905db4f357adf3d24ce
201851019-iiitv <201851019@iiitvadodara.ac.in>
2021-07-09 09:32:58 +0530

    Added code

---
+++ README.md
@@ -0,0 +1,39 @@
+# Moviespedia
+<img src= "https://visitor-badge.laobi.icu/badge?page_id=201851019-iiitv/simple-project " alt="visitor badge"/> |  ![GitHub last commit (branch)](https://img.shields.io/github/last-commit/201851019-iiitv/simple-project/master)
+
+## About 
+
+This is a website where user can get all information about any movie .When a user visit to  website and search a movies by his name then  retrieve data from `OMDB` and display all detail to user and give a button to `Know more` .If user click button then user get all details such as `Actors of this movie` ,`directors of this movie`,`Story of the movie` and much more.
+
+
+## Requirements
+
+Assumes local installation of  `NodeJS` ,`NPM` , `Postman`.
+If used windows then install  `Putty and Puttygen` .
+
+
+
+## Demo
+![moviespedia](https://user-images.githubusercontent.com/48892208/120060763-381d0800-c077-11eb-887a-a190e5594624.gif)
+[Moviespedia Server 1]( http://moviespedia.cf/)    <br>
+[Moviespedia Server2]( http://www.moviespedia.tk/)
+
+## Technologies Used
+* AWS EC2 Instances
+* NodeJS
+* ExpressJS
+* EJS
+* Bootstrap
+* HTML
+* CSS
+* Rest-API
+* Postman
+* VS Code
+* Ubuntu 18.04
+* Putty and Puttygen
+
+
+
+Thank you! ☺️
+
+made with ❤️ by Ambesh Kumar.
---
+++ app.js
@@ -0,0 +1,81 @@
+const express=require('express')
+const request=require('request')
+const app=express()
+const dotenv=require('dotenv')
+
+dotenv.config()
+app.set("view engine","ejs")
+app.get('/',(req,res)=>{
+    
+//res.send('Hompage')
+//console.log(req.params)
+//res.send("details of "+req.params.id)
+res.render("home")
+
+
+})
+
+app.get('/about',(req,res)=>{
+
+res.render("about")
+
+})
+
+
+app.get('/movies',(req,res)=>{
+
+    const url='http://www.omdbapi.com/?s='+req.query.MovieName+'&apikey='+process.env.API_KEY
+    //console.log(url)
+    //res.send("details of "+url)
+request(url,function(error,response,body){
+
+    if(!error && response.statusCode==200)
+    {
+        const data=JSON.parse(body)
+        res.render("result",{moviesData:data})
+    }
+    else{
+        res.send("Something went wrong!!!")
+    }
+
+
+
+})
+
+
+})
+
+app.get("/movies/:id" ,(req,res)=>{
+
+    const url='http://www.omdbapi.com/?i='+req.params.id+'&apikey='+process.env.API_KEY
+
+    //console.log(url)
+    //res.send("details of "+url)
+request(url,function(error,response,body){
+
+    if(!error && response.statusCode==200)
+    {
+        const data=JSON.parse(body)
+        res.render('details',{details:data})
+    }
+    else{
+        res.send("Something went wrong!!!")
+    }
+
+
+
+})   
+
+
+})
+
+
+app.get('*' ,(req,res)=>{
+    res.send("404 not found !!")
+})
+
+
+app.listen('3000',()=>{
+
+    console.log('server has started')
+})
